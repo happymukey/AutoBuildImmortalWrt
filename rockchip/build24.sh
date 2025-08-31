7220a95edbbef0ffc99fc2acf50621d4e07e8d5d
happymukey <61935898+happymukey@users.noreply.github.com>
2025-08-31 13:43:45 +0800

    Update build24.sh

--- rockchip/build24.sh
+++ rockchip/build24.sh
@@ -63,6 +63,9 @@ PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 #PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
 #PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
+PACKAGES="luci-app-ipsec-server"
+PACKAGES="luci-app-vlmcsd"
+
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
     PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
